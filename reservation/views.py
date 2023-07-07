eea8cf24d63408a8402fe8b3abc10a1d8a886771
Prasheeth <55602761+Prasheeth-Venkat-Kumar@users.noreply.github.com>
2023-07-07 17:20:48 -0400

    create views for Menuitem model

--- reservation/views.py
+++ reservation/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+# Create your views here.
+from rest_framework import generics
 
 # Create your views here.
+class MenuItemViewSet(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemViewSet(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+

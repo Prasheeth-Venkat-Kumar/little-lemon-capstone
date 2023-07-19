4a01a53edbcb9e02fcdb9364964632bcbceaf4c1
Prasheeth <55602761+Prasheeth-Venkat-Kumar@users.noreply.github.com>
2023-07-19 17:31:09 -0400

    delete reservations app and create restaurant app

--- reservation/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from rest_framework import routers
-from . import views
-
-urlpatterns = [
-    path("menuitems/", views.MenuItemViewSet.as_view()),
-    path("menuitems/<int:pk>/", views.SingleMenuItemViewSet.as_view()),
-]

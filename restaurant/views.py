a3c8cb3d7d0f9dca1ce0bba61f85ef88077dacf0
Prasheeth <55602761+Prasheeth-Venkat-Kumar@users.noreply.github.com>
2023-07-19 18:07:04 -0400

    add user auth for menuitems view

--- restaurant/views.py
+++ restaurant/views.py
@@ -4,12 +4,16 @@ from rest_framework import generics
 from .serializers import BookingSerializer, MenuItemSerializer
 # import models
 from .models import Booking, MenuItem
+# import permissions
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer

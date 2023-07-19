e4709e8a0e6d57cdd43ec23674d4af9aa7366f61
Prasheeth <55602761+Prasheeth-Venkat-Kumar@users.noreply.github.com>
2023-07-19 17:36:58 -0400

    Update models.py

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255, default="")
+    no_of_guests = models.SmallIntegerField(default=1)
+    booking_date = models.DateField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return f"{self.name} for {self.no_of_guests} guests on {self.booking_date}"
+    
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255, default="")
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    inventory = models.SmallIntegerField(default=5)

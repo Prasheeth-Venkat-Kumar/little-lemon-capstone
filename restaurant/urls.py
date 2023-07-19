32a5b630092ca646dd38e53013bc0acf1b810bd1
Prasheeth <55602761+Prasheeth-Venkat-Kumar@users.noreply.github.com>
2023-07-19 17:53:09 -0400

    add ability to get auth token, update urls.py

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,10 @@
 # import
 from django.urls import path
 from .views import MenuItemsView, SingleMenuItemView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("menu", MenuItemsView.as_view(), name="menu"),
     path("menu/<int:pk>", SingleMenuItemView.as_view(), name="single_menu_item"),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth')
 ]

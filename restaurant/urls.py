0621f6d24d27f2b3782593246736bff4b46a3e61
Prasheeth <55602761+Prasheeth-Venkat-Kumar@users.noreply.github.com>
2023-07-19 17:45:01 -0400

    create restaurant urls for menuitems

---
+++ restaurant/urls.py
@@ -0,0 +1,8 @@
+# import
+from django.urls import path
+from .views import MenuItemsView, SingleMenuItemView
+
+urlpatterns = [
+    path("menu/", MenuItemsView.as_view(), name="menu"),
+    path("menu/<int:pk>", SingleMenuItemView.as_view(), name="single_menu_item"),
+]

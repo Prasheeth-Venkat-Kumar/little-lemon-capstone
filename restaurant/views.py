72d9100f29f4937a6108726cee5c9321b03a1add
Prasheeth <55602761+Prasheeth-Venkat-Kumar@users.noreply.github.com>
2023-07-19 17:43:27 -0400

    basic views for menuitem

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+# import serializers
+from .serializers import BookingSerializer, MenuItemSerializer
+# import models
+from .models import Booking, MenuItem
 
 # Create your views here.
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
